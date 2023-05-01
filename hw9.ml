715c5bd2bf888a9c64d19a526fe915f4b0934c9b
Ian Myers <imyers@ada.gonzaga.edu>
2023-05-01 12:42:49 -0700

    Hw1.1

--- hw9.ml
+++ hw9.ml
@@ -1,8 +1,64 @@
 (*
-   Name: 
+   Name: Ian Myers
    File: hw9.ml
    Date: Spring 2023
-   Desc: 
+   Desc: 19 Functions in ocaml all doing various jobs
 *)
 
+(*Problem 1: function my_last that takes a list and returns the last element of the list*)
+let rec my_last xs =
+   match List.length xs with
+   | 0 -> failwith "Empty List"
+   | 1 -> List.hd xs
+   | _ -> my_last (List.tl xs)
+;;
 
+(*Problem 2: function my_init that takes a list and returns a new list containing all of the elements of
+the input list except for the last element*)
+let rec my_init xs = 
+   match List.length xs with
+   | 0 -> failwith "Empty List"
+   | 1 -> []
+   | _ -> List.hd xs :: (my_init (List.tl xs))
+;;
+
+(*Problem 3: function my_replace that takes a pair of values and a list and returns a new list such that
+each occurrence of the first value of the pair in the list is replaced with the second value*)
+let rec my_replace (x1,x2) xs = 
+   match xs with
+   | [] -> []
+   | x :: t when x = x1 -> x2 :: my_replace (x1,x2) t
+   | x :: t -> x :: my_replace (x1, x2) t
+;;
+
+(*Problem 4: function my_replace_all that takes a list of pairs and a list of values and returns a
+new list where each occurrence of the first value in a pair is replaced by the second value in the
+pair*)
+let rec my_replace_all xs ys =
+   match (xs, ys) with
+   | ([], ys') -> ys'
+   | (xs', []) -> []
+   | (x :: xs', _) -> my_replace_all xs' (my_replace (fst(x), snd(x)) ys)
+;;
+
+(*Problem 5:  function my_elem_sum that takes a value and a list, and returns the sum of the given values
+in the list*)
+let rec my_elem_sum x1 xs =
+   match xs with
+   | [] -> 0
+   | x :: t when x = x1 -> x + my_elem_sum x1 t
+   | x :: t -> my_elem_sum x1 t
+;;
+
+(*Problem 6: function my_range_sum v1 v2 that returns the sum of values starting at v1 and ending at v2*)
+let rec my_range_sum v1 v2 = 
+   match v1 with
+   | _ when v1 > v2 -> 0
+   | _ when v1 = v2 -> v2
+   | _ -> v1 + my_range_sum (v1 + 1) v2
+;;
+ 
+type ('a, 'b) kvlist = Node of 'a * 'b * ('a, 'b) kvlist
+| Nil ;;
+
+let insert 'a 'b ('a, 'b) kvlist = Node of ()
--- hw9_tests.ml
+++ hw9_tests.ml
@@ -1,5 +1,5 @@
 (*
-   Name: 
+   Name: Ian Myers
    File: hw9_tests.ml
    Date: Spring 2023
    Desc: Unit tests for HW9
@@ -16,4 +16,30 @@ let assert_equal v1 v2 msg =
   assert (if not cond then print_endline ("TEST FAILED: " ^ msg) ; cond)
 ;;
 
+(* Question 1: my_last tests *)
+assert_equal 3 (my_last [1; 2; 3]) "3 = my_last [1; 2; 3]";;
+assert_equal 'd' (my_last ['a'; 'b'; 'c'; 'd']) "'d' = my_last ['a'; 'b'; 'c'; 'd']";;
 
+(* Question 2: my_init tests *)
+assert_equal [1;2] (my_init [1; 2; 3]) "[1;2] = my_init [1; 2; 3]";;
+assert_equal [1;2;3;3;2] (my_init [1; 2; 3; 3; 2; 1]) "[1;2;3;3;2] = my_init [1; 2; 3; 3; 2; 1]";;
+
+(* Question 3: my_replace tests *)
+assert_equal [1;2;5] (my_replace (3,5) [1;2;3]) "[1;2;5] = my_replace (3,5) [1;2;3]";;
+assert_equal [1;2;3] (my_replace (6,5) [1;2;3]) "[1;2;3] = my_replace (6,5) [1;2;3]";;
+assert_equal [5;5;5] (my_replace (2,5) [2;2;2]) "[5;5;5] = my_replace (2,5) [2;2;2]";;
+
+(* Question 4: my_replace_all tests *)
+assert_equal [2;2;4;4] (my_replace_all [(1,2);(3,4)] [1;2;3;4]) "[2;2;4;4] = my_replace_all [(1,2);(3,4)] [1;2;3;4]";;
+assert_equal [1;2;4;5;5] (my_replace_all [(6,5);(3,4)] [1;2;3;6;5]) "[1;2;4;5;5] = my_replace_all (6,5) [1;2;3]";;
+assert_equal ['b';'b';'d';'d'] (my_replace_all [('a','b');('c','d')] ['a';'b';'c';'d']) "['b';'b';'d';'d'] = my_replace_all [('a','b');('c','d')] ['a';'b';'c';'d']";;
+
+(* Question 5: my_elem_sum tests *)
+assert_equal 3 (my_elem_sum 3 [1;2;3]) "3 = my_elem_sum 3 [1;2;3]";;
+assert_equal 0 (my_elem_sum 5 [1;2;3]) "0 = my_elem_sum 5 [1;2;3]";;
+assert_equal 10 (my_elem_sum 5 [5;2;5]) "10 = my_elem_sum 5 [5;2;5]";;
+
+(* Question 6: my_range_sum tests *)
+assert_equal 3 (my_range_sum 1 2) "3 = my_range_sum 1 2";;
+assert_equal 0 (my_range_sum 5 2) "0 = my_range_sum 5 2";;
+assert_equal 30 (my_range_sum 4 8) "30 = my_range_sum 4 8";;
